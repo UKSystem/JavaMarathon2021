a228c9e1e4989898e453bd96d199b6b944847561
UKSystem <reg@vovk.tech>
2021-04-21 16:47:33 +0300

    change day 8 task 1

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -6,20 +6,25 @@ package day8;
 public class Task1 {
     public static void main(String[] args) {
 
+        long startTime = System.currentTimeMillis();
+
         StringBuilder stringOfNumbers = new StringBuilder();
 
         for (int i = 0; i < 20001; i++) {
             stringOfNumbers.append(i).append(" ");
         }
-        System.out.println(stringOfNumbers.toString());
+        System.out.println(stringOfNumbers.toString());                                 // 15мс
 
 /*
         String stringOfNumbersExperiment = "";
         for (int i = 0; i < 20001; i++) {
             stringOfNumbersExperiment = stringOfNumbersExperiment + i + " ";
         }
-        System.out.println(stringOfNumbersExperiment);
- */
+        System.out.println(stringOfNumbersExperiment);                                  // 344мс
+*/
+
+        long timeSpent = System.currentTimeMillis() - startTime;
+        System.out.println("программа выполнялась " + timeSpent + " миллисекунд");
 
 
     }
