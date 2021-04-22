6829150763a788a05d414bac8fb5790c3834acd0
UKSystem <reg@vovk.tech>
2021-04-23 00:20:08 +0300

    add day 9

--- src/main/java/day9/Task1/Task1.java
+++ src/main/java/day9/Task1/Task1.java
@@ -3,5 +3,14 @@ package day9.Task1;
 public class Task1 {
     public static void main(String[] args) {
 
+        Student student1 = new Student("Tomas", "natural Sciences");
+        Teacher teacher1 = new Teacher("Nick", "chemistry");
+
+        System.out.println("Учебная группа " + student1.getGroupName() + ", Предмет " + teacher1.getSubjectName());
+
+        student1.printInfo();
+        teacher1.printInfo();
+
+
     }
 }
--- src/main/java/day9/Task2/TestFigures.java
+++ src/main/java/day9/Task2/TestFigures.java
@@ -3,5 +3,29 @@ package day9.Task2;
 public class TestFigures {
     public static void main(String[] args) {
 
+        double sumPerimeter = 0;
+        double sumArea = 0;
+
+        Figure[] figures = {
+                new Triangle(10, 10, 10, "Red"),
+                new Triangle(10, 20, 30, "Green"),
+                new Triangle(10, 20, 15, "Red"),
+                new Rectangle(5, 10, "Red"),
+                new Rectangle(40, 15, "Orange"),
+                new Circle(4, "Red"),
+                new Circle(10, "Red"),
+                new Circle(5, "Blue")
+        };
+
+        for (Figure i : figures) {
+
+            if (i.getColor() == "Red") {
+                sumPerimeter += i.perimeter();
+                sumArea += i.area();
+            }
+        }
+
+        System.out.println("Сумма периметров красных фигур: " + sumPerimeter);
+        System.out.println("Сумма площадей красных фигур: " + sumArea);
     }
 }
